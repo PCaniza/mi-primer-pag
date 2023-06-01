127aeb69c743f4d451a743c1060c98ac95378ef4
PCaniza <pcaniza94@gmail.com>
2023-05-31 21:39:32 -0300

    primeramodif

---
+++ script.js
@@ -0,0 +1,10 @@
+function showAlert() { alert('¡Hola, esta es una alerta desde JavaScipt!');
+}
+
+document.querySelector("button.button-menu-toggle")
+
+    .addEventListener("click", function() {
+
+           document.querySelector(".nav-links").
+
+                      classList.toggle("nav-links-responsive")})
